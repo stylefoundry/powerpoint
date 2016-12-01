d51b4e37ca8acce81668b7eda31f6e7508d15e98
Justin Boynton <justin.boynton@stylefoundry.co.uk>
2016-12-01 11:46:45 +0000

    NVIS-1304 - Add inline images
     - Add inline images to the slides from html

--- lib/powerpoint/slide/ff_trend_heading_text.rb
+++ lib/powerpoint/slide/ff_trend_heading_text.rb
@@ -2,26 +2,31 @@ require 'zip/filesystem'
 require 'fileutils'
 require 'fastimage'
 require 'erb'
+require 'mimemagic'
 
 module Powerpoint
-  module Slide
+    module Slide
     class FFTrendHeadingText
       include Powerpoint::Util
 
-      attr_reader :title, :content
+      attr_reader :title, :content, :image_paths, :images
 
       def initialize(options={})
-        require_arguments [:presentation, :title, :content], options
+        require_arguments [:presentation, :title, :content, :image_paths], options
         options.each {|k, v| instance_variable_set("@#{k}", v)}
-      end
-
-      def save(extract_path, index)
-        save_rel_xml(extract_path, index)
-        save_slide_xml(extract_path, index)
+        @images = image_paths.each.map { |image_path|  [ File.basename(image_path), image_path ] }
       end
 
       def file_type
-        nil
+        @images.map{ |image_name, image_path| { type: MimeMagic.by_magic(File.open(image_path)).type, path: "/ppt/media/#{image_name}" } }
+      end
+
+      def save(extract_path, index)
+        @images.each do |image_path, image_name|
+          copy_media(extract_path, image_path) if image_path != nil
+        end
+        save_rel_xml(extract_path, index)
+        save_slide_xml(extract_path, index)
       end
 
       def save_rel_xml(extract_path, index)
@@ -33,6 +38,7 @@ module Powerpoint
         render_view('ff_trend_heading_text_slide.xml.erb', "#{extract_path}/ppt/slides/slide#{index}.xml")
       end
       private :save_slide_xml
+
     end
   end
 end
