271dd0fc0d5d182c964e0fd5c4d7e7b91f50171e
EuanEdgar <euan@spicerack.co.uk>
2023-11-16 15:16:51 +0000

    Handle blank values in three column text component

--- lib/powerpoint/slide/ff_trend_three_row_text.rb
+++ lib/powerpoint/slide/ff_trend_three_row_text.rb
@@ -13,6 +13,42 @@ module Powerpoint
       def initialize(options={})
         require_arguments [:presentation, :title, :content, :links], options
         options.each {|k, v| instance_variable_set("@#{k}", v)}
+
+        html_wrap = -> (text) do
+          text = '&nbsp;' if text.nil? || text.empty?
+          html_to_ooxml <<~HTML
+            <p>#{text}</p>
+          HTML
+        end
+
+        html_guard_blank = -> (html) do
+          if ooxml_blank?(html)
+            html = <<~HTML
+              <p>&nbsp;</p>
+            HTML
+          end
+
+          html_to_ooxml(html)
+        end
+
+        @heading_one = html_wrap.call(
+          content.dig('column1', 'items', 'headingInput', 'value')
+        )
+        @text_one = html_guard_blank.call(
+          content.dig('column1', 'items', 'textInput', 'value')
+        )
+        @heading_two = html_wrap.call(
+          content.dig('column2', 'items', 'headingInput', 'value')
+        )
+        @text_two = html_guard_blank.call(
+          content.dig('column2', 'items', 'textInput', 'value')
+        )
+        @heading_three = html_wrap.call(
+          content.dig('column3', 'items', 'headingInput', 'value')
+        )
+        @text_three = html_guard_blank.call(
+          content.dig('column3', 'items', 'textInput', 'value')
+        )
       end
 
       def save(extract_path, index)
