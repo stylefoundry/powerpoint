7e4acd5ee80499889ba2bb1de293f9c75a807d3f
EuanEdgar <euan@spicerack.co.uk>
2024-12-10 15:40:25 +0000

    Heading text slide

--- lib/powerpoint/slide/ff_trend_heading_text.rb
+++ lib/powerpoint/slide/ff_trend_heading_text.rb
@@ -31,12 +31,12 @@ module Powerpoint
       end
 
       def save_rel_xml(extract_path, index)
-        render_view('ff_trend_heading_text_rel.xml.erb', "#{extract_path}/ppt/slides/_rels/slide#{index}.xml.rels")
+        render_view('ff_heading_text_rel.xml.erb', "#{extract_path}/ppt/slides/_rels/slide#{index}.xml.rels")
       end
       private :save_rel_xml
 
       def save_slide_xml(extract_path, index)
-        render_view('ff_trend_heading_text_slide.xml.erb', "#{extract_path}/ppt/slides/slide#{index}.xml")
+        render_view('ff_heading_text_slide.xml.erb', "#{extract_path}/ppt/slides/slide#{index}.xml")
       end
       private :save_slide_xml
 
